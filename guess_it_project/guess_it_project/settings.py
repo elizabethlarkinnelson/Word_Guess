2aa639fdda124eeb586bae7ad0c40b7cfb601f7b
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-11-17 12:10:04 -0800

    Postgres database migrated successfully

--- guess_it_project/guess_it_project/settings.py
+++ guess_it_project/guess_it_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from secret import SECRET_KEY1
+from secret import SECRET_KEY1, DB_PASS, DB_USER, DB_NAME
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'guess_it_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
