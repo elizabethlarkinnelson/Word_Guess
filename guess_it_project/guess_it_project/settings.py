39b5695ad30483032113d15fd116ac37859150aa
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-11-17 11:22:21 -0800

    Django default files

--- guess_it_project/guess_it_project/settings.py
+++ guess_it_project/guess_it_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from secret import SECRET_KEY1
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lxy6ir^i14u+cz=m)djt+k0d07++kwhz=s$y&u*lt$7(^vd%&x'
+SECRET_KEY = SECRET_KEY1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
