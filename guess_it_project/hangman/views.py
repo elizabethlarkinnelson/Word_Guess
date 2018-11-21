1da6d4bb713037b4d78b1eba1313c5415fe5c2fc
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-11-21 13:40:13 -0800

    View is able to load.  Adding static files and templates

--- guess_it_project/hangman/views.py
+++ guess_it_project/hangman/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("I'm in my project! Yipee!!")
+    return render(request, 'hangman/index.html')
