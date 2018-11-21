8159fe4077f779cb0a1df1fcf1d2e04f100c20e7
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-11-21 15:53:59 -0800

    Buttons work on index page

--- guess_it_project/hangman/urls.py
+++ guess_it_project/hangman/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'hangman'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('play/', views.play, name='play'),
 ]
--- guess_it_project/hangman/views.py
+++ guess_it_project/hangman/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'hangman/index.html')
+
+
+def play(request):
+    return render(request, 'hangman/play.html')
