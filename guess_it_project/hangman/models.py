80fde14fb88bdfadb13119e50f3877ee44d1399a
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-11-30 16:52:42 -0800

    LinkedIn API renders

--- guess_it_project/hangman/models.py
+++ guess_it_project/hangman/models.py
@@ -2,6 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class User(models.Model):
-    question_text = models.CharField(max_length=200)
--- guess_it_project/hangman/views.py
+++ guess_it_project/hangman/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+import requests
+import random
 
 
 def index(request):
-    return render(request, 'hangman/index.html')
+    response = requests.get('http://app.linkedin-reach.io/words?minLength=3&maxLength=10')
+    words = response.text.splitlines()
+
+    length = len(words)
+    random_num = random.randint(0, length)
+
+    word = words[random_num]
+    context = {'word': word}
+    return render(request, 'hangman/index.html', context)
 
 
 def play(request):
+
     return render(request, 'hangman/play.html')
