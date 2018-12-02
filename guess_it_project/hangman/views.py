7ee66f929906c602b2b75b5512462282cf346e09
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-12-01 18:29:56 -0800

    logic ip for game view

--- guess_it_project/hangman/views.py
+++ guess_it_project/hangman/views.py
@@ -17,7 +17,10 @@ def play(request):
     word = words[random_num]
     length = [i for i in range(len(word))]
 
-    new_word = Word(current_word=word, guess_number=0, letters_left=''.join(sorted(word)))
+    new_word = Word(current_word=word, guess_number=0,
+                    letters_left=''.join(sorted(word)),
+                    wrong_letters_guessed='',
+                    right_letters_guessed='')
     new_word.save()
 
     context = {'length': length,
@@ -31,14 +34,40 @@ def game(request):
     context = {}
     error = 'invalid input'
 
+    current_word = Word.objects.latest('id')
+
     if request.method == 'POST':
         guess = request.POST['guess']
         if guess.isalpha():
-            context = {'success': guess}
+            current_word.guess_number += 1
+            current_word.save()
+
+            if guess in current_word.letters_left:
+                current_word.right_letters_guessed = current_word.right_letters_guessed + guess
+                current_word.save()
+                letters_left = []
+                count = 0
+                for let in current_word.letters_left:
+                    if let == guess and count == 0:
+                        count += 1
+                        pass
+                    else:
+                        letters_left.append(let)
+                letters_left_string = ''.join(letters_left)
+                current_word.letters_left = letters_left_string
+                current_word.save()
+
+                context['right_letters'] = current_word.right_letters_guessed
+                context['letters_left'] = current_word.letters_left
+
+            else:
+                current_word.wrong_letters_guessed = current_word.wrong_letters_guessed + guess
         else:
             context = {'error': error}
 
-    context['word'] = Word.objects.latest('id').current_word
+    context['guesses'] = current_word.guess_number
+    context['guesses_left'] = 6 - context['guesses']
+    context['word'] = current_word.current_word
     context['length'] = [i for i in range(len(context['word']))]
 
     return render(request, 'hangman/game.html', context)
