d5965632d68507e5dd005236f6c46fc02b256e6f
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-12-02 19:36:11 -0800

    css arms done

--- guess_it_project/hangman/models.py
+++ guess_it_project/hangman/models.py
@@ -8,5 +8,5 @@ class Word(models.Model):
     current_word = models.CharField(max_length=11)
     guess_number = models.IntegerField()
     letters_left = models.CharField(max_length=11)
-    wrong_letters_guessed = models.CharField(max_length=6, blank=True)
+    wrong_letters_guessed = models.CharField(max_length=20, blank=True)
     right_letters_guessed = models.CharField(max_length=11, blank=True)
